0f5f8e58edb152db9fc27cb7c7d184daa05eec05
Joel David C <jdcruzzamora@gmail.com>
2021-02-25 00:34:25 -0600

    Blur y vista compuesta terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:designs/src/pages/basico_page.dart';
+import 'package:designs/src/pages/botones_page.dart';
 import 'package:designs/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-  SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+        .copyWith(statusBarColor: Colors.transparent));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
         'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(),
       },
     );
   }
